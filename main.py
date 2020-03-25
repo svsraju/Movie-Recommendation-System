8f87e266d6e22beba1f625e612fe8d36bad9c25b
Venkata Subbaraju Sagi <46058709+svsraju@users.noreply.github.com>
2020-03-24 19:34:24 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import pandas as pd
 import numpy as np
-#from math import sqrt
+from math import sqrt
 from flask import Flask, render_template, request
 # libraries for making count matrix and similarity matrix
 #from sklearn.feature_extraction.text import CountVectorizer
 #from sklearn.metrics.pairwise import cosine_similarity
 
 		
-def recommend(userinput):
+def allmovies(userinput):
 	inputMovies = pd.DataFrame(userinput)
 	genre_movies_df = pd.read_csv('genre_movies_df.csv')
-	userMovies = genre_movies_df[genre_movies_df['title'].isin(inputMovies['title'].tolist())]
+	inputId = genre_movies_df[genre_movies_df['title'].str.lower().isin(inputMovies['title'].str.lower().tolist())]
+	#Then merging it so we can get the movieId. It's implicitly merging it by title.
+	inputMovies = pd.merge(inputId, inputMovies)
+	#Dropping information we won't use from the input dataframe
+	inputMovies = inputMovies.drop('genres', 1).drop('year', 1)
+	userMovies = genre_movies_df[genre_movies_df['title'].str.lower().isin(inputMovies['title'].str.lower().tolist())]
 	genreTable = genre_movies_df.set_index(genre_movies_df['movieId'])
 	#And drop the unnecessary information
 	genreTable = genreTable.drop('movieId', 1).drop('title', 1).drop('genres', 1).drop('year', 1)
 
 	userMovies = userMovies.reset_index(drop=True)
-	userGenreTable = userMovies.drop('movieId', 1).drop('title', 1).drop('genres', 1).drop('year', 1)
-	
+	userGenreTable = userMovies.drop('movieId' , 1).drop('title', 1).drop('genres', 1).drop('year', 1)
 
 	#Dot produt to get weights
 	userProfile = userGenreTable.transpose().dot(inputMovies['rating'])
@@ -31,7 +35,7 @@ def recommend(userinput):
 	return topmovies
 
 
-def topmovies(movies):
+def topmovies(movies,userinput):
 	final_movies = []
 	for i in range(len(userinput)):
 		for j in range(len(movies['title'].values)):
@@ -43,6 +47,14 @@ def topmovies(movies):
 	final_movies = final_movies[0:11]
 	return final_movies
 	
+def checkduplicate(userInput):
+  for i in range(len(userInput)):
+    for j in range(len(userInput)):
+      if i!= j:
+        if userInput[i]['title'] == userInput[j]['title']:
+          return('This movie is not in our database.\nPlease check if you spelled it correct.')
+	
+	
 app = Flask(__name__)
 
 @app.route("/")
@@ -53,21 +65,37 @@ def home():
 def recommend():
 	userinput = []
 	key_values = {}
-        title = request.args.get('movie')
-	rating = request.args.get('rating')
-	key_values['title'] = title
-	key_values['rating'] = rating
-	userinput.append(key_values.copy())
+	titles = []
+	ratings = []
+	titles.append(request.args.get('movie'))
+	ratings.append(float(request.args.get('rating')))
+	titles.append(request.args.get('movie2'))
+	ratings.append(float(request.args.get('rating2')))
+	titles.append(request.args.get('movie3'))
+	ratings.append(float(request.args.get('rating3')))
+	titles.append(request.args.get('movie4'))
+	ratings.append(float(request.args.get('rating4')))
+	titles.append(request.args.get('movie5'))
+	ratings.append(float(request.args.get('rating5')))
+	for i in range(len(titles)):
+		key_values['title'] = titles[i]
+		key_values['rating'] = ratings[i]
+		userinput.append(key_values.copy())
 	
-	movies = userProfile(userinput)
+	print(userinput)
 	
-	r = topmovies(movies)
+	r = checkduplicate(userinput)
 	
-	return render_template('recommend.html',movie=title,r=r)
-        #if type(r)==type('string'):
-        	#return render_template('recommend.html',movie=title,r=r,t='s')
-    	#else:
-        	#return render_template('recommend.html',movie=title,r=r,t='l')
+	if type(r)==type('string'):
+		return render_template('recommend.html',r=r,t='s')
+    
+	else:
+		movies = allmovies(userinput)
+	
+		r = topmovies(movies,userinput)
+   
+		return render_template('recommend.html',r=r, t='l')
+        
 
 
 
